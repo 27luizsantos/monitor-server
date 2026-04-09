4f469494acef483b5358b764950cf33f70e42b0a
27LuizSantos <105029004+27LuizSantos@users.noreply.github.com>
2026-04-09 10:06:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 from flask import Flask, request, jsonify
+from datetime import datetime
 
+dados_lojas = {}
 app = Flask(__name__)
 
 @app.route("/status", methods=["POST"])
 def status():
     data = request.json
-    print("📩 Recebido:", data, flush=True)  # 👈 CORREÇÃO
+
+    loja = data.get("loja")
+    agora = datetime.now().strftime("%H:%M:%S")
+
+    dados_lojas[loja] = {
+        "dados": data.get("dados"),
+        "ultima_atualizacao": agora
+    }
+
+    print("📩 Recebido:", data, flush=True)
+
     return jsonify({"message": "OK"}), 200
 
-@app.route("/")
-def home():
-    return "Servidor rodando!", 200
+
+@app.route("/painel")
+def painel():
+    html = "<h1>Painel de Monitoramento</h1>"
+
+    for loja, info in dados_lojas.items():
+        status = "🟢 ONLINE" if info["dados"]["ativo"] else "🔴 OFFLINE"
+
+        html += f"""
+        <p>
+        <b>{loja}</b><br>
+        Status: {status}<br>
+        Última atualização: {info['ultima_atualizacao']}
+        </p>
+        <hr>
+        """
+
+    return html
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
