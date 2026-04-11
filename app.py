6941c7f6970ab942792a7e6686b5ed93aa9208e5
27LuizSantos <105029004+27LuizSantos@users.noreply.github.com>
2026-04-11 14:24:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def painel():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Monitoramento VR</title>
+        <title>Monitoramento VR-Concentrador</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
         <meta http-equiv="refresh" content="30">
